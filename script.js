c2ba8f3c32e8b99dea513a88a3600e2a3cd1789c
rachel <yesrjv@gmail.com>
2023-10-17 11:25:12 -0700

    added notes and more variables

--- script.js
+++ script.js
@@ -12,11 +12,36 @@ function writePassword() {
 
 }
 
+var lowercase = ["a","b","c"]
+var uppercase = ['A',"B","C"]
+var numbers = ["1", "2", "3"]
+var special = ["#", "@", "$"]
+
+var chosenCharacters = []
+var finalPassword =[]
 function generatePassword() {
+  // takes user input value 
+  var passwordLength = window.prompt("how long would you like your password to be? password must be between 8-128 characters")
+
+  var confirmLowerCase = window.confirm("do you want lowercase letters in your password?")
+  var confirmUpperCase = window.confirm("do you want uppercase letters in your password?")
+  var numbers = window.confirm("do you want to use numbers in your password?")
+  var special = window.confirm("do you want to use special characters in your password?")
+
+if (confirmLowerCase === true) {
+  chosenCharacters = chosenCharacters.concat (lowercase)
+}
+// as above for other character types
 
 
 
 
+
+console.log(chosenCharacters)
+// loop oer chosenCharacters array for as many times as the user answers for passwordLength and push random chacter from chasenCharacters into finalPassword array
+
+
+// insert joined finalPAssword array over "hello"
   return "hello"
 }
 
